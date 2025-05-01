5155fa42dd3a49a8d29ac5beadde47757be2d83d
Kavya-114 <kavya11420@gmail.com>
2025-05-01 22:26:03 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -9,15 +9,14 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-2"
   access_key = var.access_key
   secret_key = var.secret_key
 }
 
 # create security group for the ec2 instance
-resource "aws_security_group" "ec2_security_group2" {
-  name        = "ec2 security group3"
-  vpc_id      = "vpc-0c1fb0e7610cb1508"
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2 security group"
   description = "allow access on ports 22"
 
   # allow access on port 22
@@ -37,16 +36,4 @@ resource "aws_security_group" "ec2_security_group2" {
   }
 
   tags = {
-    Name = "Monitoring server security group"
-  }
-}
-
-resource "aws_instance" "Monitoring_server" {
-ami = "ami-0e35ddab05955cf57"  
-instance_type = "t2.medium"
-security_groups = [aws_security_group.ec2_security_group.name]
-key_name = var.key_name
-tags = {
-  Name: var.instance_name
-}
-}
+    Name = "Monitoring server security group"…
